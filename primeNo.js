3c5b8bdc0fef4487036454e2a3ed26d4442799a1
alfiya1310 <74983960+alfiya1310@users.noreply.github.com>
2022-01-26 19:52:13 +0530

    Update primeNo.js

--- primeNo.js
+++ primeNo.js
@@ -1,18 +1,17 @@
-while(true)
-{
+while (true) {
     let num = Math.floor(Math.random() * (100 - 1)) + 1;
-    if(isNotPrime(num)) console.log(num);
-    else
-    {
+    if (isNotPrime(num)) {
+        console.log(num);
+    }
+    else {
         console.log(num);
         break;
     }
 }
 
-function isNotPrime(num)
-{
-    for(var i = 2; i < num; i++) {
-        if(num % i === 0) {
+function isNotPrime(num) {
+    for (var i = 2; i < num; i++) {
+        if (num % i === 0) {
             return true;
         }
     }
