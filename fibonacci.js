cf27cc81b88c4de3df15ec894538b8ff9da32c16
alfiya1310 <74983960+alfiya1310@users.noreply.github.com>
2022-01-26 19:48:51 +0530

    Add files via upload

---
+++ fibonacci.js
@@ -0,0 +1,21 @@
+function fibonacci(num)
+{
+    let array1 = [];
+	let num1 = 0;
+	let num2 = 1;
+	let temp;
+	for (let i = 1; i <= num; i++)
+	{
+	    array1.push(num1);
+		temp = num1 + num2;
+		num1 = num2;
+		num2 = temp;
+	}
+	return array1;
+}
+
+let ans = fibonacci(10);
+for(element of ans)
+{
+    console.log(element);
+}
---
+++ tableOf5.js
@@ -0,0 +1,12 @@
+function tableOf5(num,i) 
+{
+    return `${num} * ${i} = ${num*i}`;
+}
+
+let num = 5;
+let length = 10;
+
+for(let i = 1; i <= length; i++)
+{
+    console.log(tableOf5(num,i));
+}
