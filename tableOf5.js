e22acc1d7bb23978825b07fe7345331d58035d20
alfiya1310 <74983960+alfiya1310@users.noreply.github.com>
2022-01-26 19:52:54 +0530

    Update tableOf5.js

--- tableOf5.js
+++ tableOf5.js
@@ -1,12 +1,10 @@
-function tableOf5(num,i) 
-{
+function tableOf5(num,i) {
     return `${num} * ${i} = ${num*i}`;
 }
 
 let num = 5;
 let length = 10;
 
-for(let i = 1; i <= length; i++)
-{
+for (let i = 1; i <= length; i++) {
     console.log(tableOf5(num,i));
 }
